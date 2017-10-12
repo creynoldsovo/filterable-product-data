17751d4c53a2c6eba38bc1d349b49ed34e11b893
Chris Reynolds <chris.reynolds@ovoenergy.com>
2017-10-12 11:54:48 +0100

    Add scss file for App Refactor FilterableProductTable

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
 
-import { ProductRow } from './ProductRow';
+import { ProductRow } from './ProductRow/ProductRow';
 
 export class FilterableProductTable extends Component {
 
-    render() {
-      return (
-        <table className="filterable-products__table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>Category</th>
-            </tr>
-            {
-              this.props.products.map((product) => 
-                <ProductRow product={ product } />
-              )
-            }
-          </tbody>
-        </table>
-      );
-    }
+  generateKeyId(keyName, item) {
+    // return `${ keyName }-${ item.replace(' ', '-').toLowerCase() }`;
+    return keyName + '-' + item.replace(' ', '-').toLowerCase();
   }
+
+  render() {
+
+    const rows = [];
+    let category;
+
+    this.props.products.forEach((product) => {
+
+      if (product.category !== category) {
+
+        rows.push(
+          <tr key={ this.generateKeyId('category', product.category) }>
+            <th colSpan="2">{ product.category }</th>
+          </tr>
+        )
+      }
+
+      rows.push(
+        <ProductRow
+          key={ this.generateKeyId('product', product.name) }
+          name={ product.name }
+          price={ product.price }
+          stocked={ product.stocked } />
+      );
+
+      category = product.category;
+    });
+
+    return (
+      <table className="filterable-products__table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          { rows }
+        </tbody>
+      </table>
+    );
+  }
+}
